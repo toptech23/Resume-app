422ce28972f0a9f1cdd3da51c71efe1f0555bc52
TechJob <Toptech23@users.noreply.github.com>
2024-01-31 13:43:22 +0100

    My second commit

---
+++ index.php
@@ -0,0 +1,84 @@
+<html>
+<div id="header"></div>
+<div class="left"></div>
+<div class="stuff">
+  <br><br>
+  <h1>Resume</h1>
+  <h2>Temitope Emmanuel Oni</h2>
+  <h3>Email: geomantee1@gmail.com |Phone: 08067054671 |linkedln:linkedin.com/in/temitope-oni-53761235/</h3>
+  <h4>Summary</h4>
+  <hr />
+  <br>
+   <h4>I am a results-oriented DevOps Engineer with three-years track record in orchestrating end-to-end CI/CD pipelines. Highly proficient in leveraging industry-leading tools such as Jenkins, Docker, and Kubernetes to automate, streamline, and optimize software development processes</h4>
+  <hr />
+  <br>
+  <p class="head">Interests</p>
+  <ul>
+    <li>Reading</li>
+    <li>Football</li>
+    <li>Cloud Computing</li>
+    <li>Programming</li>
+    <li>Information Technology</li>
+    <li>Music</li>
+   <hr />
+   <br>
+  </ul>
+  <p class="head">Skills</p>
+  <ul>
+    <li>Cloud Computing</li>
+    <li>Azure Virtual Machine</li>
+    <li>Azure Blob & File Storage</li>
+    <li>Azure Virtual Network</li>
+    <li>Azure Monitor</li>
+    <li>Azuer App Service</li>
+    <hr />
+    <br>
+    </ul>
+    <p class="head">Projects</p>
+    </ul>
+    <li>Design, implement, and maintain robust CI/CD pipelines, emphasizing the use of Docker for containerization.</li>
+    <li>Collaborate with cross-functional teams to troubleshoot, optimize, and enhance Docker-based solutions.</li>
+    <li>Implement Docker best practices for security, performance, and resource efficiency.</li>
+  <p class="head">Education</p>
+    <li>Manage and optimize networking configurations on AWS, ensuring secure and efficient communication between services.</li>
+    <li>Implement Infrastructure as Code (IaC) principles using Terraform for provisioning and managing AWS resources.</li>
+     <li>Design, deploy, and maintain containerized applications on Kubernetes clusters, ensuring scalability and reliability.</li>
+  <ul>
+    <a href="http://www.futa.edu.ng">
+      <li>Msc Federal University of Technology, Akure 2014-2016 </li>
+    </a>
+    <!--Link-->
+    <a href="https://www.fedpolyado.edu.ng/">
+      <li>HND Federal Polytechnic, Ado Ekiti 2006-2008</li>
+    </a>
+    <li>Ekiti Baptist High School, Igede Ekiti 1996-2002</li>
+  <hr />
+  <br
+  </ul>
+  <p class="head">Experience</p>
+  <ul>
+  <li>Lecturer Federal Polytechnic</li>
+  <li>Graduate Intern, Geoterrain Nigeria Ltd, Lekki Lagos</li>
+   <li>Field Geoscientist, Geoshore Consult, Ado Ekiti</li>
+  <li>Physics Teacher, Ikigbinsin High School</li>
+  <hr />
+  <br
+  </ul>
+  <p class="head">Languages</p>
+  <li>English - Proficient</li>
+  <li>Yoruba - Proficient</li>
+  <hr />
+  <br
+  </ul>
+  <p class="head">Extra Curriculars</p>
+  <ul>
+    <li>Traveling</li>
+    <li>Reading</li>
+    <li>Table Tennis</li>
+  </ul>
+   <p class="head">Refences</p>
+   <li>Upon request</li>
+</div>
+<div class="right"></div>
+<div id="footer">
+  <h2 id="name">Temitope</h2></div>
